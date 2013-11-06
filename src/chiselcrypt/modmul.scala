6caecbae599a78549f1a7001e91c6ebf4a683eaa
Kevin Linger <linger@berkeley.edu>
2013-11-06 13:16:15 -0800

    debugging mod multiply

--- src/chiselcrypt/modmul.scala
+++ src/chiselcrypt/modmul.scala
@@ -22,21 +22,29 @@ class modMultiply extends Module {
 	val io = new modMultiplyIO
 
 
-	//Registers, using variable notation from McIvor Algorithms 1 and 2
+	//Registers
 	val op1 = Reg(Bits(0, 256))
 	val op2 = Reg(Bits(0, 256))
 	val modulo = Reg(Bits(0, 256))
 	val op1Mod = Reg(Bits(0, 256))
 	val op2Mod = Reg(Bits(0, 256))
+	val accumReg = Reg(init = Bits(0, 256))
 	val product = Reg(Bits(0, 512))
 	val flag1 = Reg(Bool(false))
 	val flag2 = Reg(Bool(false))
+
+	val bigMod = Reg(Bits(0, 512))
+
+	val op1shift = Reg(Bits(0, 256))
+	val op2shift = Reg(Bits(0, 256))
+
+	val prodShift = Reg(Bits(0, 512))
 
 	//Default values of outputs
 	io.control_resp_data := Bits(0, 256)
 
 	//For some reason I have to do states this way, Enum isn't working for me, even though I'm doing the exact same things as I did in lab???
-	val state = Reg(UInt(0, 3))
+	val state = Reg(init = UInt(0, 3))
 
 	//Valid when you are in the finish state
 	io.control_resp_val := (state === UInt(3))
@@ -47,11 +55,18 @@ class modMultiply extends Module {
 			op1 := io.control_req_op1
 			op2 := io.control_req_op2
 			modulo := io.control_req_modulo
-
 			op1Mod := io.control_req_op1
 			op2Mod := io.control_req_op2
 
+			op1shift := io.control_req_op1 >> UInt(1)
+			op2shift := io.control_req_op2 >> UInt(1)
+		
+			bigMod := io.control_req_modulo 
+
+			accumReg := Bits(0, 256)
 			product := Bits(0, 512)
+			flag1 := Bool(false)
+			flag2 := Bool(false)
 
 			state := UInt(1)
 		}
@@ -59,15 +74,25 @@ class modMultiply extends Module {
 
 	//To do A*B mod C, first do AmodC and BmodC
 	when(state === UInt(1)){
-		when(op1Mod >= modulo){
-			op1Mod := op1Mod - modulo
-		}
 
+		
+	//	when(op1shift >= modulo){
+		when((op1Mod >> UInt(1)) >= modulo){
+			op1Mod := op1Mod >> UInt(1)
+		//	op1shift := op1shift >> UInt(1)
+		}	
+		.elsewhen(op1Mod >= modulo){
+			op1Mod := op1Mod - modulo
+		//	op1shift := op1shift >> UInt(1)
+		}
 		.otherwise{
 			flag1 := Bool(true)
 		}
 
-		when(op2Mod >= modulo){
+		when((op2Mod >> UInt(1)) >= modulo){
+			op2Mod := op2Mod >> UInt(1)
+		}
+		.elsewhen(op2Mod >= modulo){
 			op2Mod := op2Mod - modulo
 		}
 
@@ -77,13 +102,20 @@ class modMultiply extends Module {
 
 		when(flag1 & flag2){
 			product := op1Mod*op2Mod
+			prodShift := (op1Mod*op2Mod) >> UInt(1)
 			state := UInt(2)
 		}
+
 	}
+
 
 	//Now do (AmodC*BmodC) mod C
 	when(state === UInt(2)){
-		when(product >= modulo){
+		when(prodShift >= modulo){
+			product := product >> UInt(1)
+			prodShift := prodShift >> UInt(1)
+		}
+		.elsewhen(product >= modulo){
 			product := product - modulo
 		}
 		.otherwise{
